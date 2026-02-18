42e8d0738bc9030e33453d2570f94bcc4b33907b
hedidheyaa-collab <hedidheyaa@gmail.com>
2026-02-18 11:33:31 +0100

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,11 @@
+# 🏪 Gestion de Kiosque
+
+Application de gestion de stock et de ventes pour kiosque, développée avec *React*.
+
+### ✨ Fonctionnalités :
+* 📦 *Gestion des stocks* en temps réel (alertes si stock bas).
+* 🛒 *Panier d'achat* rapide avec calcul de la monnaie.
+* 📝 *L'Ardoise* : Système de suivi des dettes clients.
+* 📊 *Statistiques* : Chiffre d'affaires et Top 3 des produits.
+* 📱 *Export WhatsApp* : Envoi direct du ticket de caisse.
+*
